f22e43c185594bb869a0a5ba3d8a589b2e09d4c0
Michael Langer <mail@michaellanger.me>
2024-05-01 18:16:52 +0200

    fixes ktlint findings

--- app/src/main/java/de/mchllngr/devsettings/extension/flow/FlowExt.kt
+++ app/src/main/java/de/mchllngr/devsettings/extension/flow/FlowExt.kt
@@ -8,9 +8,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
-// see https://medium.com/androiddevelopers/repeatonlifecycle-api-design-story-8670d1a7d333
-/* Copyright 2022 Google LLC.
-   SPDX-License-Identifier: Apache-2.0 */
+/* 
+ * Copyright 2022 Google LLC.
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * see https://medium.com/androiddevelopers/repeatonlifecycle-api-design-story-8670d1a7d333
+ */
 inline fun <T> Flow<T>.launchAndCollectIn(
     owner: LifecycleOwner,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
