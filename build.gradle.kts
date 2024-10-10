474e9e3276ab39d161793ae10c50b3a092a515ee
Michael Langer <michael.langer@check24.de>
2024-10-10 18:22:15 +0200

    [feature/target-sdk] sets target sdk to 35

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
